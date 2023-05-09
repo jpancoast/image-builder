e7b788bcac5021abda9c0ffc0c6324ab85b19e67
James Pancoast <jamesp@jpancoast.dev>
2023-05-08 22:18:31 -0600

    blah

--- lxc/alpine/n8n/provision.sh
+++ lxc/alpine/n8n/provision.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Installing nodejs & npm"
 apk add nodejs npm
 
 echo "Node Version:"
@@ -8,4 +9,9 @@ node -v
 echo "npm version:"
 npm -v
 
+#
+#   SURPRISE! This seems to be causing issues with permisions or changing
+#       permisions or something
+#
+echo "Installing n8n"
 npm install n8n -g
