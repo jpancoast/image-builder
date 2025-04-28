e47d8d62181c2835185ab6f35f93d3867172eddc
James Pancoast <jamesp@jpancoast.dev>
2025-04-27 19:51:21 -0600

    blah

--- lxc/alpine/pihole/provision.sh
+++ lxc/alpine/pihole/provision.sh
@@ -71,19 +71,45 @@ git clone --depth 1 https://github.com/pi-hole/pi-hole.git /etc/.pihole
 
 mkdir -p /var/www/html /etc/pihole /etc/dnsmasq.d
 
-git clone --depth 1 https://github.com/pi-hole/AdminLTE.git /var/www/html/admin
+# Clone the AdminLTE repository
+#git clone --depth 1 https://github.com/pi-hole/AdminLTE.git /var/www/html/admin
+git clone --depth 1 https://github.com/pi-hole/AdminLTE.git /var/www/localhost/htdocs
 
-touch /etc/pihole/setupVars.conf
+# Copy Pi-hole files
+cp /etc/.pihole/gravity.sh /usr/local/bin/
+cp /etc/.pihole/pihole /usr/local/bin/
 
-echo 'conf-dir=/etc/dnsmasq.d/,*.conf' > /etc/dnsmasq.conf
+cat > /etc/pihole/setupVars.conf << EOF
+PIHOLE_INTERFACE=eth0
+IPV4_ADDRESS=0.0.0.0
+IPV6_ADDRESS=
+QUERY_LOGGING=true
+INSTALL_WEB=true
+DNSMASQ_LISTENING=local
+DNS_FQDN_REQUIRED=true
+DNS_BOGUS_PRIV=true
+DNSSEC=false
+TEMPERATUREUNIT=F
+WEBUIBOXEDLAYOUT=traditional
+WEBPASSWORD=a450425a9da3c27f3430ed153d19e9a647f7f6ad2e11f5836cd10a9c2b94d6c3
+BLOCKING_ENABLED=true
+PIHOLE_DNS_1=8.8.8.8
+PIHOLE_DNS_2=8.8.4.4
+EOF
 
-sh -c 'echo "conf-dir=/etc/dnsmasq.d/,*.conf" > /etc/dnsmasq.conf'
+cat > /etc/dnsmasq.d/01-pihole.conf << EOF
+addn-hosts=/etc/pihole/gravity.list
+localise-queries
+no-resolv
+no-poll
+no-negcache
+cache-size=10000
+local-ttl=2
+EOF
 
 rc-update add dnsmasq default
+rc-update add lighttpd default
+
 /etc/init.d/dnsmasq start
 /etc/init.d/lighttpd start
-
-
-
-
 
